12969cc65573a81f4ac4ee54bbaa17496c68e71f
Ursula <20100902@mail.wit.ie>
2023-02-02 16:55:03 +0000

    Rounding to 2 decimal places using a helper function in the main class.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -12,12 +12,15 @@ val prsi_Percentage = 5.2;
 val annual_Bonus = 1_450.50;
 val cws_Monthly_Deduction = 54.33;
 
-val df = DecimalFormat("#.##")
 
 fun monthly(number : Double) =  number/12
 fun fromPercentage(percentage: Double) : Double{
     return (percentage * (monthlySalary/100))
 }
+fun round(number: Double): String {
+    return "%.2f".format(number)
+}
+
 val monthlySalary : Double = monthly(gross_Salary)
 val monthly_Bonus : Double = monthly(annual_Bonus)
 val paye = fromPercentage(paye_Percentage)
@@ -28,13 +31,13 @@ val deduction = cws_Monthly_Deduction + paye + prsi
 
 fun main() {
     println("\t\t Monthly Pay Slip \n\n")
-    println("Employee Name: $firstName $surname($gender) \t\t Employee ID: $employee_Id \n\n")
+    println("Employee Name: $firstName $surname(${gender.uppercase()}) \t\t Employee ID: $employee_Id \n\n")
     println("PAYMENT DETAILS \t\t\t  DEDUCTIONS DETAILS \n")
-    println("Salary: $monthlySalary \t\t PAYE: $paye \n")
-    println("Bonus: $monthly_Bonus \t\t\t PRSI: $prsi \n")
-    println("\t\t\t\t Cycle To Work: $cws_Monthly_Deduction \n\n")
-    println("Gross: $gross \t\t Total Deductions: $deduction \n\n")
-    println("\t\t NET PAY: ${gross - deduction}")
+    println("Salary: ${round(monthlySalary)} \t\t\t\t PAYE: ${round(paye)} \n")
+    println("Bonus: ${round(monthly_Bonus)} \t\t\t\t\t PRSI: ${round(prsi)} \n")
+    println("\t\t\t\t Cycle To Work: ${round(cws_Monthly_Deduction)} \n\n")
+    println("Gross: ${round(gross)} \t\t Total Deductions: ${round(deduction)} \n\n")
+    println("\t\t NET PAY: ${round(gross - deduction)}")
 }
 
 
